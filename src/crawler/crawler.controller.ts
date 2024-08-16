2a3ac117a4d1c356667a41b7990d290a40e477eb
josedossantos10 <josedossantos@outlook.com>
2024-08-16 15:19:46 -0300

    Adicionar modulo do Crawler no projeto

---
+++ src/crawler/crawler.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { Link } from 'src/utils/intefaces/link.interface';
+import { CrawlerService } from './crawler.service';
+
+@Controller('crawler')
+export class CrawlerController {
+  constructor(private readonly crawlerService: CrawlerService) {}
+
+  @Post('sitemap')
+  async obterLinksSitemap(@Body() body: { url: string }) {
+    return this.crawlerService.obterLinksSitemap(body.url);
+  }
+
+  @Post('rastrear')
+  async rastrearLinks(
+    @Body() body: { urls: string[]; maxProfundidade: number },
+  ) {
+    return this.crawlerService.rastrearLinks(body.urls, body.maxProfundidade);
+  }
+
+  @Post('testar')
+  async testarLinks(@Body() body: Link[]) {
+    return this.crawlerService.testarLinks(body);
+  }
+}
---
+++ src/crawler/crawler.service.ts
@@ -0,0 +1,99 @@
+import { ProcessedRequest, UnprocessedRequest } from '@crawlee/types';
+import { Injectable } from '@nestjs/common';
+import { gotScraping, PlaywrightCrawler, Sitemap } from 'crawlee';
+import { Link } from 'src/utils/intefaces/link.interface';
+
+@Injectable()
+export class CrawlerService {
+  ALLOW_EXTENSIONS = ['pdf', 'csv', 'ods', 'xls'];
+  async removerDuplicados(data: any) {
+    const uniqueLinks: Link[] = [];
+    data.forEach((link: any) => {
+      if (link?.uniqueKey?.length > 0) {
+        const newObj = {} as Link;
+        const ext = link.uniqueKey.split('.').pop();
+        newObj.url = link.uniqueKey;
+        newObj['ext'] =
+          ext.length < 5 && this.ALLOW_EXTENSIONS.includes(ext) ? ext : '';
+
+        if (uniqueLinks.findIndex((x) => x.url === newObj.url) === -1) {
+          uniqueLinks.push(newObj);
+        }
+      }
+    });
+    return uniqueLinks;
+  }
+  async rastrearLinks(
+    url: string[],
+    maxProfundidade: number = 10,
+  ): Promise<Link[]> {
+    const linksP: ProcessedRequest[][] = [];
+    const linksU: UnprocessedRequest[][] = [];
+    let result: Link[] = [];
+
+    const crawler = new PlaywrightCrawler({
+      async requestHandler({ enqueueLinks }) {
+        const res = await enqueueLinks({
+          skipNavigation: false,
+          strategy: 'all',
+        });
+        linksP.push(res.processedRequests);
+        linksU.push(res.unprocessedRequests);
+      },
+      maxRequestRetries: 1,
+      maxRequestsPerCrawl: maxProfundidade,
+    });
+
+    await crawler.run(url);
+
+    const combinedLinks = [...linksU, ...linksP];
+    combinedLinks.forEach((link: any) => {
+      result.push({ url: link.url, ext: '', status: '' });
+    });
+    result = await this.removerDuplicados(result);
+    console.log(
+      `Númuero de links encontados: ${combinedLinks.length}. Links únicos: ${result.length}`,
+    );
+
+    return result;
+  }
+
+  async obterLinksSitemap(url: string): Promise<Link[]> {
+    const urls = (await Sitemap.load(url)).urls;
+    const links: Link[] = urls.map((url) => ({ url, ext: '', status: '' }));
+
+    return links;
+  }
+
+  async rastrearLinksSitemap(url: string): Promise<Link[]> {
+    const crawler = new PlaywrightCrawler({
+      async requestHandler({ request, log }) {
+        log.info(request.url);
+      },
+      maxRequestsPerCrawl: 10, // Limitar a quantidade de requisições para 10 (0 para ilimitado)
+    });
+
+    const { urls } = await Sitemap.load(url);
+    await crawler.addRequests(urls);
+
+    await crawler.run();
+
+    return urls.map((url) => ({ url, ext: '', status: '' }));
+  }
+
+  async testarLinks(data: Link[]): Promise<Link[]> {
+    // const result: Link[] = [];
+    const result: Link[] = [];
+    for (const link of data) {
+      try {
+        const res: any = await gotScraping({ url: link.url });
+        link['status'] = res.statusCode;
+        result.push(link);
+        // console.log('Success: ', link);
+      } catch (error) {
+        // console.error('Error: ', error);
+      }
+    }
+    return result;
+  }
+}
