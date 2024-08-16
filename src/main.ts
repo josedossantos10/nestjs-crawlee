10ba99c138f56d10232b34ca056fc35a6e77e568
josedossantos10 <josedossantos@outlook.com>
2024-08-16 16:41:50 -0300

    Adicionar mensagem de inicio da aplicação

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.disable('x-powered-by');
+  app.disable('X-Powered-By');
   await app.listen(3004);
+
+  const serverUrl = 'http://localhost:3004';
+
+  console.log(`
+
+  🚀 Aplicação iniciada!
+
+  📚 Acesse a documentação Swagger em: ${serverUrl}/documentation
+
+  🌐 Aplicação disponível em: ${serverUrl}
+  `);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('❌ Ocorreu um erro ao iniciar a aplicação:', error);
+});
